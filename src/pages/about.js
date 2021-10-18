977f57494051466a77df8da773ea27e3acfa5237
amberdonnelly <amberdonnelly@alumni.ubc.ca>
2021-10-18 16:49:57 -0400

    site skeleton

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+// styles
+import '../scss/about.scss';
+
+// data
+
+// markup
+const AboutPage = () => {
+    return (
+      <main>
+        <title>about</title>
+        <p>Hello</p>
+      </main>
+    )
+  }
+  
+  export default AboutPage
---
+++ src/pages/portfolio.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+// styles
+import '../scss/portfolio.scss';
+
+// data
+
+// markup
+const PortfolioPage = () => {
+    return (
+      <main>
+        <title>portfolio</title>
+        <p>Hello</p>
+      </main>
+    )
+  }
+  
+  export default PortfolioPage
