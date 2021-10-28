0da78f750c6420c35f258e9e1a38e284573dc61b
amberdonnelly <amberdonnelly@alumni.ubc.ca>
2021-10-28 18:37:07 -0400

    add project cards

--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -1,13 +1,55 @@
 import * as React from "react"
-import { Container } from "react-bootstrap"
+import { Container, Col, Button } from "react-bootstrap"
 
 // styles
 import '../scss/sections.scss';
 
+// images
+import RobotImg from "../images/robot.png"
+
+// data
+import * as projects from '../utils/projects.js'
+
+// constants
+const image_map  = {
+    'robot': RobotImg,
+}
+
+// components
+function Card(props) {
+    const { project } = props;
+    return(
+      <a className="card" target="_blank" rel="noreferrer" href={project.link}>
+        <div className="card__body">
+            <p className="card__title">{project.title}</p>
+            <p className="card__description">{project.description}</p>
+            <div className="card__skills">
+                {project.skills.map( (skill) => {
+                    return(
+                        <div className="card__skills-badge">{skill}</div>
+                    )
+                })}
+            </div>
+        </div>
+        <div className="card__media">
+            <img className="card__media-img" src={image_map[project.image]} />
+        </div>
+      </a>
+    )
+}
+
 const PortfolioSection = () => {
     return (
         <Container className="portfolio">
-            <p>projects will go here :)</p>
+            <Container className="projects">
+                {projects.PROJECTS.map( (project) => {
+                    return(
+                        <Col className="projects__col">
+                            <Card project={project} />
+                        </Col>
+                    )
+                })}
+            </Container>
         </Container>
     )
 }
---
+++ src/utils/projects.js
@@ -0,0 +1,47 @@
+export const PROJECTS = [
+    {
+        'title': 'autonomous robot',
+        'description': 'designed and built from scratch for the ubc enph (engineering physics) 253 course',
+        'skills': [
+            'arduino (C)',
+            '3D modelling',
+            'laser cutting',
+            'waterjet cutting',
+            'circuit planning',
+            'soldering',
+            'html/css',
+        ],
+        'image': 'robot',
+        'link': 'https://channel4robotteam.github.io/index.html'
+    },
+    // {
+    //     'title': 'autonomous robot',
+    //     'description': 'designed and built from scratch for the ubc enph (engineering physics) 253 course',
+    //     'skills': [
+    //         'arduino (C)',
+    //         '3D modelling',
+    //         'laser cutting',
+    //         'waterjet cutting',
+    //         'circuit planning',
+    //         'soldering',
+    //         'html/css',
+    //     ],
+    //     'image': 'robot',
+    //     'link': 'https://channel4robotteam.github.io/index.html'
+    // },
+    // {
+    //     'title': 'autonomous robot',
+    //     'description': 'designed and built from scratch for the ubc enph (engineering physics) 253 course',
+    //     'skills': [
+    //         'arduino (C)',
+    //         '3D modelling',
+    //         'laser cutting',
+    //         'waterjet cutting',
+    //         'circuit planning',
+    //         'soldering',
+    //         'html/css',
+    //     ],
+    //     'image': 'robot',
+    //     'link': 'https://channel4robotteam.github.io/index.html'
+    // }
+]
